26b053494cb466c596df13568a9930962373b277
marovichn <nikolamarovic.11@gmail.com>
2023-04-21 00:38:58 +0200

    timeouts-states-settings

--- animations/src/components/Modal/Modal.js
+++ animations/src/components/Modal/Modal.js
@@ -3,9 +3,37 @@ import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 500,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={500} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      onEnter={() => {
+        console.log("Enter");
+      }}
+      onEntering={() => {
+        console.log("Entering");
+      }}
+      onEntered={() => {
+        console.log("Entered");
+      }}
+      onExit={() => {
+        console.log("Exit");
+      }}
+      onExiting={() => {
+        console.log("Exiting");
+      }}
+      onExited={() => {
+        console.log("Exited");
+      }}
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
