ddb78a20583fe81379be313c79e96353ff7eff59
marovichn <nikolamarovic.11@gmail.com>
2023-04-21 00:11:57 +0200

    Transition-component

--- animations/src/App.js
+++ animations/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -23,12 +24,39 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button className="Button">Toggle</button>
-        {this.state.showBlock ? (
-          <div
-            style={{ backgroundColor: "red", width: 100, height: 100 }}
-          ></div>
-        ) : null}
+        <button
+          onClick={() => {
+            this.setState((prev) => {
+              return { showBlock: !prev.showBlock };
+            });
+          }}
+          className="Button"
+        >
+          Toggle
+        </button>
+        <br></br>
+        <Transition
+          in={this.state.showBlock}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => {
+            return (
+              <div
+                style={{
+                  backgroundColor: state === "entered" ? "green" : "red",
+                  width: 100,
+                  height: 100,
+                  margin: "auto",
+                  transition: "opacity 500ms ease-out",
+                  opacity: state === "exiting" ? 0 : 1,
+                }}
+              ></div>
+            );
+          }}
+        </Transition>
+
         {this.state.modalIsOpen && (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         )}
